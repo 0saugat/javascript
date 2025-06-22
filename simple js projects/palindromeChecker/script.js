10755b2b4ca55a36b5110b14b72381d5a67cd7dc
Saugat <pdl.saugat07@gmail.com>
2025-06-22 17:50:59 +0545

    Update JavaScipt to check palindrome

---
+++ simple js projects/palindromeChecker/script.js
@@ -0,0 +1,27 @@
+const inputText = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const resultText = document.getElementById("result");
+
+const checkPalindrome = () => {
+  const currInput = inputText.value;
+  if (currInput !== "") {
+    const regex = /[.,\s]/g;
+    const userInput = currInput.replace(regex, "").toLowerCase();
+    console.log(userInput);
+    const inputArr = userInput.split("");
+    const palinArr = [];
+    for (let i = inputArr.length - 1; i >= 0; i--) {
+      let letter = inputArr.pop();
+      palinArr.push(letter);
+    }
+    palinText = palinArr.join("");
+    if (userInput === palinText) {
+      resultText.innerText = `${currInput} is a palindrome`;
+    } else {
+      resultText.innerText = `${currInput} is not a palindrome`;
+    }
+  } else {
+    alert("Please input a value");
+  }
+};
+checkButton.addEventListener("click", checkPalindrome);
